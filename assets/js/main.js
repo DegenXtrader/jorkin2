2160cd790cd472cca22bf15ad3277bb6200a97e2
Degen Comedian <degen.comedian@gmail.com>
2024-12-23 13:23:40 +0100

    first working version with iframes

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,4 +96,6 @@ const sr = ScrollReveal({
 sr.reveal(`.home-swiper, .howtobuy-swiper, .newsletter__container`)
 sr.reveal(`.category__data, .trick__content, .footer__content`,{interval: 100})
 sr.reveal(`.about__data, .discount__img`,{origin: 'left'})
+sr.reveal(`.generator__data`,{origin: 'left'})
+sr.reveal(`.gallery__data`,{origin: 'left'})
 sr.reveal(`.about__img, .discount__data`,{origin: 'right'})
